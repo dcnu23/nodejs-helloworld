a294c5229b970d9adc7b948e00c6e4c0469a3127
dcnu23 <deekonda.srinivas@gmail.com>
2018-02-04 11:43:58 -0500

    updates to hello world

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+var PORT=process.env.PORT;
 var express = require('express');  // http server 
 var app = express();
 
-app.listen(3000,function(){  // creates application server at port 3000 and has call back function
+//app.listen(3000,function(){  // creates application server at port 3000 and has call back function
+app.listen(PORT,function(){  
     console.log("Server up and running ,i am on port 3000");
 });
 
